151810d59311da64f52b5f2cbdc5f4a778fc54ce
Alona <polejcukalona@gmail.com>
2023-08-09 00:28:40 +0300

    asdasd

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,20 @@
+const startBtn = document.querySelector('[data-start]');
+let intervalId;
 
+startBtn.addEventListener('click', startColorSwitcher);
+document.querySelector('[data-stop]').addEventListener('click', stopColorSwitcher);
+
+
+function getRandomHexColor() {
+  document.body.style.backgroundColor = `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
+}
+
+function startColorSwitcher() {
+  startBtn.disabled = true;
+  intervalId = setInterval(getRandomHexColor, 1000);
+}
+
+function stopColorSwitcher() {
+  startBtn.disabled = false;
+  clearInterval(intervalId);
+}
